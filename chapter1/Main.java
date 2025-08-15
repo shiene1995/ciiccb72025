5a4993ee2d8a90dd037c742796f9ddc9490f5292
Kenneth Salvador <salvadorkennethmontemayor@gmail.com>
2025-08-16 00:56:51 +0800

    suggetion code

--- chapter1/Main.java
+++ chapter1/Main.java
@@ -20,5 +20,15 @@ public class Main{
         System.out.println(r.nextInt(100));
         ArrayList al = new ArrayList<>();
         Date date1 = new Date();
+
+        echo("This is testing from K.Salvador"); //String
+        echo(123); //int
+        echo(98.5); //double
+        echo(true); //boolean
     }
+
+    static void echo(String data){System.out.println(data);} //shorten the System.out.println() to echo()
+    static void echo(int data){System.out.println(data);}
+    static void echo(double data){System.out.println(data);}
+    static void echo(boolean data){System.out.println(data);}
 }
