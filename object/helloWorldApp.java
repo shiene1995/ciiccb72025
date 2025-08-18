56f683af0d69d5a0f56ce45fc77661b09fde2c2e
Kenneth Salvador <salvadorkennethmontemayor@gmail.com>
2025-08-18 15:59:52 +0800

    self check

---
+++ object/helloWorldApp.java
@@ -0,0 +1,11 @@
+package object;
+
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Mundo!"); // Display the string.
+    }
+}
